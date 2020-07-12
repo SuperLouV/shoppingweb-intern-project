ec94cd39bc0648faa4559505f7a41543226e1ec1
francisfeng <13903131445@163.com>
2020-07-12 10:57:58 -0400

    add http error handler

---
+++ src/main/java/com/dropshyp/shoppingweb/errorHandler/RestErrorResponse.java
@@ -0,0 +1,27 @@
+package com.dropshyp.shoppingweb.errorHandler;
+
+public class RestErrorResponse {
+    private Integer errorCode;
+    private String errorMsg;
+
+    public RestErrorResponse(Integer errorCode, String errorMsg) {
+        this.errorCode = errorCode;
+        this.errorMsg = errorMsg;
+    }
+
+    public Integer getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(Integer errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+}
