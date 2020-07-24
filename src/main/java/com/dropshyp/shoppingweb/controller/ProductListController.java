9e62fbd824d63df8df50c79da796769d4236689a
louyilin <louyilin1996@gmail.com>
2020-07-23 21:30:29 -0400

    7.23 Add bootstrap-fileinput used to upload pictures 2. Product page can get data from backend

--- src/main/java/com/dropshyp/shoppingweb/controller/ProductListController.java
+++ src/main/java/com/dropshyp/shoppingweb/controller/ProductListController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,11 +22,21 @@ public class ProductListController {
     private ProductListService productListService;
 
     @GetMapping("/post-ad")
-    public String post(Model model) {
+    public String get(Model model) {
+        /**
+        * @Description: all_contact_name from all suppliers
+        * @Param: [model]
+        * @return: java.lang.String
+        * @Author: Yilin Lou
+        * @Date: 7/23/20
+        */
         List<String> all_contact_name = productListService.findAllContact_name();
-        model.addAttribute(all_contact_name);
+        model.addAttribute("all_contact_name",all_contact_name);
         System.out.println(all_contact_name);
         return "post-ad";
     }
+
+
+
 
 }
